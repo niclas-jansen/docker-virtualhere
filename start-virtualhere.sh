ab3bfd4da1aa34001a21f9ef96482bc5837e5791
Niclas Jansen <niclas.jansen@gmail.com>
2021-07-10 04:02:10 +0200

    add check for already installed zerotier

--- start-virtualhere.sh
+++ start-virtualhere.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -e
 #
 # Script used to setup and run virtualhere
-curl -s https://install.zerotier.com | sudo bash
-sudo zerotier-cli join $NETWORK_ID
-sudo zerotier-cli listnetworks
+if ! [ -x "$(command -v zerotier-cli)" ]; then
+    curl -s https://install.zerotier.com | sudo bash
+    sudo zerotier-cli join $NETWORK_ID
+    sudo zerotier-cli listnetworks
+else
+    sudo zerotier-cli listnetworks
+fi
 
 
 ARCH=`uname -m`
